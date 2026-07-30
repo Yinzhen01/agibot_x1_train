bdffed67780d8eeddf1b1ea52dfdd4cc7f22a296
Yinzhen01 <yinzhen0101@163.com>
2026-07-30 13:58:51 +0800

    tune: 加强脚踝Roll提前制动

--- humanoid/envs/x1/x1_dh_stand_config.py
+++ humanoid/envs/x1/x1_dh_stand_config.py
@@ -377,7 +377,7 @@ class X1DHStandCfg(LeggedRobotCfg):
             dof_pos_limits = -10.
             dof_torque_limits = -0.1
             ankle_pitch_braking = -50.
-            ankle_roll_braking = -50.
+            ankle_roll_braking = -100.
 
     class normalization:
         class obs_scales:
